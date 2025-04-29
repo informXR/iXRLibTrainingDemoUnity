07082e4de0f479be4d46321b277cfd0526cd92c1
Nick Ramirez <nick.ramirez@arborxr.com>
2025-04-29 12:00:28 -0700

    Add ReAuth box

---
+++ Assets/Scripts/ReAuthenticateButton.cs
@@ -0,0 +1,10 @@
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ReAuthenticateButton : XRBaseInteractable
+{
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        Abxr.ReAuthenticate();
+    }
+}
